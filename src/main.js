0ef2137725676fdaf773e0467f1392f29ce4d50d
Delsea12 <dellasetyowati12@gmail.com>
2023-06-05 23:10:47 +0700

    pemweb

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import './assets/style.css'
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
+
+
+
+// TODO: Replace the following with your app's Firebase project configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC9AOQv6LEpF5O8HEuOyZbBdq3ke6EcEPM",
+    authDomain: "hafozh-dfdbb.firebaseapp.com",
+    projectId: "hafozh-dfdbb",
+    storageBucket: "hafozh-dfdbb.appspot.com",
+    messagingSenderId: "489628069659",
+    appId: "1:489628069659:web:ad50dddc10db19b0b6adbe",
+    measurementId: "G-RKFCFR85XW"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+createApp(App).use(router).mount("#app");
+
+export { app, auth };
---
+++ src/router/index.js
@@ -0,0 +1,55 @@
+import { createRouter, createWebHistory } from "vue-router";
+import LoginView from "../components/login.vue";
+import Redirect from '../views/Redirect.vue';
+import { getAuth } from "firebase/auth";
+
+const routes = [
+  {
+    path: "/",
+    name: "login",
+    component: LoginView,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../components/register.vue"),
+  },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../components/dashboard.vue"),
+    meta: {
+      authRequired: true,
+    },
+  },
+  {
+    path: '/:code',
+    name: 'Redirect',
+    component: Redirect
+  }
+  
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const auth = getAuth();
+
+  if (to.matched.some((record) => record.meta.authRequired)) {
+    if (auth.currentUser) {
+      next();
+    } else {
+      alert("You've must been logged to access this area");
+      router.push("/");
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
